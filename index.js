2fb50de0ae8f943ebef976a124a7680ca817a5c7
Ahmed Mahmoud <56558577+AhmedAbouelkher@users.noreply.github.com>
2024-06-24 14:36:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3333;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, World from the other side\n');
 });
 
 server.listen(port, hostname, () => {
